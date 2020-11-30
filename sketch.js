c4e386a7f0f75dd836c2c0f6b001da959ffd0545
promit1201 <bharpromit@gmail.com>
2020-11-30 15:23:41 +0530

    added a platform

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg;
+var platForm;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +18,8 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    
+    platForm = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,5 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    platForm.display();
 }
